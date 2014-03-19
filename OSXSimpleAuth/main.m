7e1f40b2ca8eb9b7fd461341b7815a18646a079c
mjt0704 <mjt0704@gmail.com>
2014-03-19 16:26:57 +0800

    Initial Commit

---
+++ OSXSimpleAuth/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  OSXSimpleAuth
+//
+//  Created by Maojie Tang on 14-3-19.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
